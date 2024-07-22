03c63382e39941afac60aa31c9d0311b2552c919
Konfx18a <akademiax18a@gmail.com>
2024-07-22 12:49:53 +0300

    Update Module2hard.py

--- Module2/Module2hard.py
+++ Module2/Module2hard.py
@@ -1,9 +1,10 @@
+#
 first_stown = int(input('Число на первом камне?: '))
 pswd = []
 mas = [i for i in range(1, first_stown)]
 while mas != []:
     for j in range(1, len(mas)):
-        if not first_stown % mas[0] + mas[j]:
+        if not first_stown % (mas[0] + mas[j]):
             pswd += [mas[0]] + [mas[j]]
     del mas[0]
 print(pswd)
