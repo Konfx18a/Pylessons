e8983167b3fc0d4c54bcbc886fceb1862ad39726
Konfx18a <akademiax18a@gmail.com>
2024-07-22 12:55:09 +0300

    Update Module_2_4.py

--- Module2/Module_2_4.py
+++ Module2/Module_2_4.py
@@ -1,6 +1,6 @@
-import randUnivers as rU
-# numbers = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-numbers = rU.rInt(100 , 20)
+# import randUnivers as rU
+numbers = [1, 2, 3, 4, 5 , 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+# numbers = rU.rInt(100 , 20)
 prost = []
 neprost = []
 for i in numbers:
