84090d21090affe5169b89217164356b87a3101a
Konfx18a <akademiax18a@gmail.com>
2024-07-22 13:07:03 +0300

    Update Module_2_5.py

--- Module2/Module_2_5.py
+++ Module2/Module_2_5.py
@@ -1,7 +1,17 @@
-import randUnivers as rU
-def get_matrix():
+# import randUnivers as rU
+#def get_matrix():
+#    matrix=[]
+#    for i in range(1,10):
+#        matrix += [rU.rInt(100,10)]
+#    print (matrix)
+#get_matrix()
+def get_matrix(n, m, value):
     matrix=[]
-    for i in range(1,10):
-        matrix += [rU.rInt(100,10)]
+    while n:
+        row= []
+        for j in range(0,m):
+            row.append(value)
+        matrix.append(row)
+        n -= 1
     print (matrix)
-get_matrix()
+get_matrix(5,4,10)
