17ce59d1a8d2cb99720e364d245c74fe4da1f54d
Konfx18a <akademiax18a@gmail.com>
2024-09-08 14:55:52 +0300

    Add files via upload

--- Module_14/Module_14_5.py
+++ Module_14/Module_14_5.py
@@ -1,94 +1,130 @@
-import arcade
-import numpy as np
-import cfg
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+from aiogram.dispatcher import FSMContext
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+import asyncio
+from random import randint
+import logging
+import texts
+from keyboards import *
+from config import *
+from crud_functions import *
+# import types.CallbackQuery
+bot = Bot(token=API)
+dp = Dispatcher(bot, storage=MemoryStorage())
+logging.basicConfig(level=logging.INFO)
 
-# Класс
-
-class GridWorld:
-
-    def __init__(self):
-    # Инициализация Q-таблицы
-        self.Q = np.zeros((cfg.GRID_SIZE[0], cfg.GRID_SIZE[1], len(cfg.ACTIONS)))
-
-    # Функции для выбора действий и обновления Q-таблицы
-    def choose_action(self, state):
-        if np.random.rand() < cfg.EPSILON:
-            return np.random.randint(len(cfg.ACTIONS))
-        else:
-            return np.argmax(self.Q[state])
-
-    @staticmethod
-    def collision(self, stat, rect_grid):
-        if stat in cfg.STOWNS or stat not in rect_grid.keys():
-            return True
-
-    def step(self, state, action, rects_grid):
-        next_state = (state[0] + cfg.ACTIONS[action][0], state[1] + cfg.ACTIONS[action][1])
-        # if next_state[0] < 0 or next_state[0] >= grid_size or next_state[1] < 0 or next_state[1] >= grid_size:
-        if self.collision(next_state, rects_grid):
-            next_state = state  # оставаться на месте, если выходит за пределы
-        reward = 1 if next_state == cfg.LABY_OUTPUT else -0.1
-        done = next_state == cfg.LABY_OUTPUT
-        return next_state, reward, done
-
-    def update_q(self, state, action, reward, next_state):
-        best_next_action = np.argmax(self.Q[next_state])
-        td_target = reward + cfg.GAMMA * self.Q[next_state][best_next_action]
-        td_error = td_target - self.Q[state][action]
-        self.Q[state][action] += cfg.ALFA * td_error
+class RegistrationState(StatesGroup):
+    username = State()
+    email = State()
+    age = State()
+    balance = State(state='1000')
 
 
-class StalkerZone(arcade.Window):
-
-    def __init__(self, window_x, window_y, title, grid=cfg.GRID_SIZE, stowns=cfg.STOWNS,
-                 bg_color=cfg.BG_COLOR, staker_cur_cell=cfg.LABY_INPUT):
-        super().__init__(window_x, window_y, title)
-        self.stalker_cur_cell = staker_cur_cell
-        self.grid = grid
-        self.stowns = stowns
-        self.window_x = window_x
-        self.window_y = window_y
-        self.bg_color = bg_color
-        self.title = title
-        rect_size = int((min(window_x, window_y) - 100) / max(self.grid))
-        self.rects_dict = {(i, j): (50 + rect_size * (i+0.5), 50 + rect_size * (j+0.5), rect_size, rect_size)
-                          for i in range(0, self.grid[0]) for j in range(0, self.grid[1])}
-        self.setup()
-
-    def setup(self):
-        # arcade.start_render()
-        self.clear((255,255,255))
-        self.draw_zone()
-
-    def draw_zone(self):
-        for cords, rect in self.rects_dict.items():
-            if cords in self.stowns:
-                arcade.draw_rectangle_filled(*rect, (127, 127, 127))
-            else:
-                arcade.draw_rectangle_filled(*rect, (0, 255, 0))
+@dp.message_handler(commands=["start"])
+async def start_(message):
+    await message.answer(texts.start + f' {message.from_user.username}', reply_markup= start_kb)
 
 
-    def draw_stalker(self, cell):
-        arcade.draw_rectangle_filled(self.rect_dict[self.stalker_cur_cell], (0, 255, 0))
-        arcade.draw_circle_filled(self.rect_dict[cell][0], self.rect_dict[cell][1],
-                                self.rect_dict[cell][2], (255, 0, 0))
-        arcade.stalker_cur_cell = cell
+@dp.message_handler(text='О нас')
+async def about_(message):
+    await message.answer(texts.about)
+
+@dp.message_handler(text='Регистрация')
+async def reg_username(message):
+    await message.answer('Введите имя пользователя')
+    await RegistrationState.username.set()
+
+@dp.message_handler(state=RegistrationState.username)
+async def set_email(message, state):
+    if is_included(message.text):
+        await message.answer('Пользователь с таким именем существует, введите другое !')
+        return
+    await state.update_data(username=message.text)
+    await message.answer('Введите email')
+    await RegistrationState.email.set()
+
+@dp.message_handler(state=RegistrationState.email)
+async def set_age(message, state):
+    await state.update_data(email=message.text)
+    await message.answer('Введите возраст')
+    await RegistrationState.age.set()
+
+@dp.message_handler(state=RegistrationState.age)
+async def set_balance(message, state):
+    # await state.update_data(age=message.text)
+    try:
+        await state.update_data(age=int(message.text))
+    except ValueError:
+        await message.answer('Возраст должен быть числом')
+        return
+    await message.answer('Введите баланс, при вводе не числового значения будет значение по умолчанию (1000)')
+    await RegistrationState.balance.set()
+
+@dp.message_handler(state=RegistrationState.balance)
+async def set_all_data(message, state):
+    try:
+        await state.update_data(balance=int(message.text))
+    except ValueError:
+        await state.update_data(balance=1000)
+    data = await state.get_data()
+    add_user(data['username'], data['email'], data['age'], int(data['balance']))
+    await message.answer('Регистрация прошла успешно!')
+    state.finish()
+
+@dp.message_handler(text='Купить')
+async def get_buying_list(message):
+    all_products_ = get_all_products()
+    len_ = get_db_lenght()
+    for i in range(0, len_):
+        with open(f'img/img{i+1}.jpg', 'rb') as photo:
+            await message.answer(f'Название: {all_products_[i][1]} | Описание: {all_products_[i][2]} '
+                                 f'| Цена: {all_products_[i][3]}')
+            await message.answer_photo(photo)
+    # await message.answer('Выберите продукт для покупки', reply_markup=photo_catalog_kb)
+
+@dp.callback_query_handler(text = 'product_buying')
+async def send_confirm_message(call):
+    await call.message.answer('Вы успешно приобрели продукт!')
+    await call.answer()
+
+@dp.message_handler(text='Стоимость')
+async def price_(message):
+    await message.answer('Выберите интересующий товар', reply_markup=catalog_kb)
+
+@dp.callback_query_handler(text = 'baby')
+async def buy_baby(call):
+    await call.message.answer(texts.baby_bicycle, reply_markup=buy_kb)
+    await call.answer()
+
+@dp.callback_query_handler(text = 'hardtail')
+async def buy_hard(call):
+    await call.message.answer(texts.hardTail_bicycle, reply_markup=buy_kb)
+    await call.answer()
+
+@dp.callback_query_handler(text = 'BMX')
+async def buy_bmx(call):
+    await call.message.answer(texts.BMX_bicycle, reply_markup=buy_kb)
+    await call.answer()
+
+@dp.callback_query_handler(text = 'MTB')
+async def buy_MTB(call):
+    await call.message.answer(texts.MTB_bicycle, reply_markup=buy_kb)
+    await call.answer()
+
+@dp.callback_query_handler(text='back_to_catalog')
+async def back_to_catalog(call):
+    await call.message.answer('Что вам интересно?', replay_markup=catalog_kb)
+    await call.answer()
+
+
 
 
 if __name__ == '__main__':
-    gw = GridWorld()
-    stz = StalkerZone(cfg.WINDOW_X, cfg.WINDOW_Y, cfg.SCREEN_TITLE)
-    arcade.run()
-    # Обучение агента
-    for episode in range(cfg.EPISODZ):
-        cur_state = cfg.LABY_INPUT
-        done = False
-        while not done:
-            action = gw.choose_action(cur_state)
-            next_state, reward, done = gw.step(cur_state, stz.rects_dict)
-            stz.draw_stalker(next_state)
-            gw.update_q(cur_state, action, reward, next_state)
-            state = next_state
+    executor.start_polling(dp, skip_updates=True)
+
 
 
 
