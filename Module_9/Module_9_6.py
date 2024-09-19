c0401c9560f0ffa984d8ead5837d4940e3c74b84
Konfx18a <akademiax18a@gmail.com>
2024-09-19 21:06:28 +0300

    Update Module_9_6.py

--- Module_9/Module_9_6.py
+++ Module_9/Module_9_6.py
@@ -1,5 +1,5 @@
-# NB!!
-def all_variants(text):
+# Вариант с while
+def all_variants_with_while(text):
     i = 0
     j = 1
     step = 1
@@ -12,11 +12,25 @@ def all_variants(text):
             continue
         i += 1
         j = i + step
+# Вариант с for
 
+def all_variants_with_for(text):
+    size = len(text)
+    for step in range(1, size+1):
+        for cursor in range(size):
+            if step+cursor > size:
+                break
+            yield text[cursor:step+cursor]
 
-obj = all_variants('abcdefghijk')
+obj = all_variants_with_while('abcdefghijk')
+obj1 = all_variants_with_for('abcdefghijk')
 
+print('# Вариант с while)
 for i in obj:
+    print(i)
+
+print('# Вариант с for")
+for i in obj1:
     print(i)
 
 
