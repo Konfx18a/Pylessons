3c9b8415a48acb015474a2527d26ced0d4217996
Konfx18a <akademiax18a@gmail.com>
2024-09-19 20:16:22 +0300

    Update db.py

--- Module_17/app2/app/backend/db.py
+++ Module_17/app2/app/backend/db.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, DeclarativeBase
 from sqlalchemy import Integer, String, Column
 
-# engine = create_engine("sqlite:///ecommerce.db", echo= True)
 engine = create_engine('sqlite:///taskmanager.db', echo=True)
 SessionLocal = sessionmaker(bind=engine)
 
