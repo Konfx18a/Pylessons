e152d30eef2206d6cd4f3710e0b4b666ce8d1958
Konfx18a <akademiax18a@gmail.com>
2024-09-19 20:19:34 +0300

    Update main.py

--- Module_17/app2/app/main.py
+++ Module_17/app2/app/main.py
@@ -10,6 +10,5 @@ app.get("/")
 async def welcome():
     return {"message": "Welcome to Taskmanager"}
 
-app.include_router(category.router)
 app.include_router(task.task_router)
 app.include_router(user.user_router)
