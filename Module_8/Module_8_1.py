aa0c7d36d756a689dac84e3385d6e072b7aa64bf
Konfx18a <akademiax18a@gmail.com>
2024-09-19 08:23:11 +0300

    Update Module_8_1.py

--- Module_8/Module_8_1.py
+++ Module_8/Module_8_1.py
@@ -1,10 +1,9 @@
 def add_everything_up(a,b):
     try:
-        c=a+b
+        return a+b
     except TypeError as exte:
         return str(a)+str(b)
-    else:
-        return a+b
+    
 
 print(add_everything_up('sd',5))
 print(add_everything_up(5,1.234))
