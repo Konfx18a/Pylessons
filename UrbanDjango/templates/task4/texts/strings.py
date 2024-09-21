7f598f6ea0c5276248cceb1e38f6fb55179231d2
Konfx18a <akademiax18a@gmail.com>
2024-09-21 13:19:31 +0300

    Add files via upload

---
+++ UrbanDjango/templates/task4/texts/strings.py
@@ -0,0 +1,16 @@
+main_page = {
+'main': 'Главная',
+'shop': "Магазин",
+'basket': "Корзина"
+}
+shop= {
+'title': "Игры",
+'button': "Купить",
+'button_back': "Вернуться обратно",
+'games': ["Pacman", "Doom I", "Tetris"],
+        }
+basket = {
+
+'no_products': "Ваша корзина пуста",
+'button_back': "Вернуться обратно"
+        }
