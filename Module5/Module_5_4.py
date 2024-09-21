ba0de7f66bda5040c5d913696f48e142b76339e3
Konfx18a <akademiax18a@gmail.com>
2024-09-21 19:09:31 +0300

    Update Module_5_4.py

--- Module5/Module_5_4.py
+++ Module5/Module_5_4.py
@@ -1,20 +1,3 @@
-# !!!!!!!!!!!!!!!!!!!!!!! ПЕРЕПИСАТЬ
-
-
-# class Example:
-#   def __new__(cls, *args, **kwargs):
-#     print(args)
-#     print(kwargs)
-#     return object.__new__(cls)
-#
-#   def __init__(self, first, second, third):
-#     print(first)
-#     print(second)
-#     print(third)
-#
-# ex = Example('data', second=25, third=3.14)
-
-
 from random import randint
 
 
