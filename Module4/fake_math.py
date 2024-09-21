e245a54cb39f76d84c94c4fbd2ac54af90327e17
Konfx18a <akademiax18a@gmail.com>
2024-09-21 19:02:46 +0300

    Add files via upload

---
+++ Module4/fake_math.py
@@ -0,0 +1,5 @@
+def divide(first, secound):
+    if secound == 0:
+        return 'Ошибка'
+    else:
+        return first/secound
---
+++ Module4/true_math.py
@@ -0,0 +1,6 @@
+from math import inf
+def divide(first, secound):
+    if secound == 0:
+        return inf
+    else:
+        return first/secound
