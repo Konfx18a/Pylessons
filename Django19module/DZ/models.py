1043f5588f2899fde87b35a3f3075a2e6d258de0
Konfx18a <akademiax18a@gmail.com>
2024-09-23 19:03:04 +0300

    Update models.py

--- Django19module/DZ/models.py
+++ Django19module/DZ/models.py
@@ -5,6 +5,6 @@ from django.db import models
 class Books(models.Model):
     Title = models.CharField(max_length=100)
     Description = models.TextField()
-
+    id = models.IntegerField(primary_key=True)
     def __str__(self):
         return self.Title
